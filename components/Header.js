aadee61934bb580f384784f3656c3375ce3f9287
mauro <mvenicios@bol.com.br>
2023-04-30 22:50:18 -0300

    add search bar to the header component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import VidaSnap from "../images/vidasnaplogo.png";
-//import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
-//import { HomeIcon } from "@heroicons/react/solid";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 //import { useSession } from "next-auth/react";
 
 export default function Header() {
@@ -34,7 +34,7 @@ export default function Header() {
         {/*Middle*/}
         <div className="relative mt-1">
           <div className="absolute top-2 left-2">
-            {/* <SearchIcon className="h-5 text-gray-500" /> */}
+            <SearchIcon className="h-5 text-gray-500" />
           </div>
           <input
             type="text"
@@ -44,8 +44,8 @@ export default function Header() {
         </div>
         {/*Right*/}
         <div className="flex space-x-4 items-center">
-          {/* <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" /> */}
-          {/* <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" /> */}
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
           <img
             src="https://s2.glbimg.com/bidN4ro-uEHAEJABM4Jx2bRO31w=/0x0:600x900/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_ba3db981e6d14e54bb84be31c923b00c/internal_photos/bs/2021/s/E/X5IaJTRRS3AcI1TFPYSw/2019-11-06-gettyimages-501312488.jpg"
             alt="Richar Gere"
