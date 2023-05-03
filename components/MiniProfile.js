47b0648b4dd7be54f21d7c60b9871b21bc4a28d6
mauro <mvenicios@bol.com.br>
2023-05-02 22:29:51 -0300

    modify mini profile and story components to include the session

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,17 +1,25 @@
+import { useSession, signOut } from "next-auth/react";
+
 export default function MiniProfile() {
+  const { data: session } = useSession();
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 rounded-full border p-[2px]"
-        src="https://s2.glbimg.com/bidN4ro-uEHAEJABM4Jx2bRO31w=/0x0:600x900/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_ba3db981e6d14e54bb84be31c923b00c/internal_photos/bs/2021/s/E/X5IaJTRRS3AcI1TFPYSw/2019-11-06-gettyimages-501312488.jpg"
+        src={session?.user.image}
         alt="usuario"
       />
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">maurosouza</h2>
+        <h2 className="font-bold">{session?.user.username}</h2>
         <h3 className="text-sm text-gray-400">Bem vindo ao VidaSnap</h3>
       </div>
       <div className="">
-        <button className="font-semibold text-blue-400 text-sm">Sair</button>
+        <button
+          onClick={signOut}
+          className="font-semibold text-blue-400 text-sm"
+        >
+          Sair
+        </button>
       </div>
     </div>
   );
