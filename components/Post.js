b7fc570c0445a41e19c2bf50ee05a9b8be09dba7
mauro <mvenicios@bol.com.br>
2023-05-07 18:54:27 -0300

    show the number of likes

--- components/Post.js
+++ components/Post.js
@@ -108,6 +108,9 @@ export default function Post({ img, userImg, caption, username, id }) {
 
       {/*Post comments*/}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} gostei</p>
+        )}
         <span className="font-bold mr-2">{username}</span>
         {caption}
       </p>
